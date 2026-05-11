5a483ae908e74d4c932e409e728bfff054755e12
Kartik Saini <kackysaini8@gmail.com>
2026-05-11 08:23:15 +0530

    restrucutre

---
+++ assg-1/target/debug/deps/assign_1-230e504e4bd54363.d
@@ -0,0 +1,5 @@
+D:\Newfolder\turbin3\assign-1\target\debug\deps\assign_1-230e504e4bd54363.d: src\main.rs
+
+D:\Newfolder\turbin3\assign-1\target\debug\deps\libassign_1-230e504e4bd54363.rmeta: src\main.rs
+
+src\main.rs:
---
+++ assg-1/target/debug/deps/assign_1-a6bde110c274c341.d
@@ -0,0 +1,5 @@
+D:\Newfolder\turbin3\assign-1\target\debug\deps\assign_1-a6bde110c274c341.d: src\main.rs
+
+D:\Newfolder\turbin3\assign-1\target\debug\deps\libassign_1-a6bde110c274c341.rmeta: src\main.rs
+
+src\main.rs:
